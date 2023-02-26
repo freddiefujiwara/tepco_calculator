be6aeecbeffbfaa9730fe0daa84c721d849de406
Fumikazu Fujiwara | Freddie <github@ze.gs>
2023-02-26 00:28:03 +0000

    first commit

---
+++ tepco_calculator.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+import math
+
+# Fuel cost adjustment and renewable energy surcharge are prices as of March 2021
+fuel_cost = Decimal("-4.85") # JPY/kWh
+renewable_energy = Decimal("2.98") # JPY/kWh
+
+# Basic rate table (per contracted capacity)
+basic_charge = {10: Decimal("286.00"), 15: Decimal("429.00"), 20: Decimal("572.00"), 30: Decimal("858.00"), 40: Decimal("1144.00"), 50: Decimal("1430.00"), 60: Decimal("1716.00")}
+basic_charge.setdefault(0,0)
+
+# Table of electricity charges (per usage)
+power_charge = {120: Decimal("19.88"), 300: Decimal("26.48")}
+
+def basic_charge_calc(capacity):
+    """Calculate basic charge (per month)"""
+    return basic_charge.get(capacity, 0)
+
+def power_charge_calc(usage):
+    """Calculate electricity charges (per month)"""
+    if  usage <= 120:
+        return usage * power_charge[120]
+    elif usage <= 300:
+        return 120 * power_charge[120] + (usage - 120) * power_charge[300]
+    else:
+        return 120 * power_charge[120] + 180 * power_charge[300] + (usage - 300) * Decimal("30.57")
+
+def fuel_cost_calc(usage):
+    """Calculate fuel cost adjustment (per month)"""
+    return usage * fuel_cost
+
+def renewable_energy_calc(usage):
+    """Calculate renewable energy surcharge (per month)"""
+    return usage * renewable_energy
+
+def total_calc(capacity, usage):
+    """Calculate total charges (per month)"""
+    basic_cost = basic_charge_calc(capacity)
+    if basic_cost == 0:
+        return 0
+    if usage <= 0:
+        return 0
+
+    return math.ceil(basic_cost + power_charge_calc(usage) + fuel_cost_calc(usage) + renewable_energy_calc(usage))
---
+++ tepco_calculator_test.py
@@ -0,0 +1,41 @@
+import tepco_calculator
+import unittest
+from decimal import Decimal
+
+class TestTepcoCalculator(unittest.TestCase):
+    def test_scenarios(self):
+        self.assertEqual(tepco_calculator.total_calc(5, 100), 0)
+        self.assertEqual(tepco_calculator.total_calc(40, -10), 0)
+
+    def test_basic_charge_calc(self):
+        self.assertEqual(tepco_calculator.basic_charge_calc(10), 286.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(15), 429.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(20), 572.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(30), 858.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(40), 1144.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(50), 1430.00)
+        self.assertEqual(tepco_calculator.basic_charge_calc(60), 1716.00)
+
+    def test_power_charge_calc(self):
+        self.assertEqual(tepco_calculator.power_charge_calc(100), Decimal('1988.00'))
+        self.assertEqual(tepco_calculator.power_charge_calc(120), Decimal('2385.60'))
+        self.assertEqual(tepco_calculator.power_charge_calc(150), Decimal('3180.00'))
+        self.assertEqual(tepco_calculator.power_charge_calc(300), Decimal('7152.00'))
+
+    def test_fuel_cost_calc(self):
+        self.assertEqual(tepco_calculator.fuel_cost_calc(100), Decimal('-485.00'))
+        self.assertEqual(tepco_calculator.fuel_cost_calc(120), Decimal('-582.00'))
+        self.assertEqual(tepco_calculator.fuel_cost_calc(300), Decimal('-1455.00'))
+
+    def test_renewable_energy_calc(self):
+        self.assertEqual(tepco_calculator.renewable_energy_calc(100), Decimal('298.00'))
+        self.assertEqual(tepco_calculator.renewable_energy_calc(120), Decimal('357.60'))
+
+    def test_total_calc(self):
+        self.assertEqual(tepco_calculator.total_calc(40, 360), 9457)
+        self.assertEqual(tepco_calculator.total_calc(40, 120), 3306)
+        self.assertEqual(tepco_calculator.total_calc(40, 300), 7735)
+        self.assertEqual(tepco_calculator.total_calc(40, 301), 7764)
+
+if __name__ == '__main__':
+    unittest.main()
